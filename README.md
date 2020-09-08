feec74f926f79f130aebbc033f22d2421a262fb4
LizzyModau26 <modaulebogang@gmail.com>
2020-09-08 15:20:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-lizzyhotel
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-lizzyhotel)
---
+++ src/app/_models/user.ts
@@ -0,0 +1,18 @@
+import { Role } from './role';
+
+export class User {
+    id: string;
+    title: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    room: string;
+    occup: string;
+    days: string;
+    cate: string;
+    numb: string;
+    card: string;
+    exp: string;
+    cvv: string;
+    role: Role
+}
